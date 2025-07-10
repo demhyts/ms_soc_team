f96a8cae09ece4cb1ee3551d5e45b63a320640c5
demhyts <aldyth.m@jayasakara.com>
2025-07-10 17:38:01 +0700

    Create checkvt_hash.sh
    Check VT Hash

---
+++ checkvt_hash.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#set -e
+#apikey="5e92bfe394fe33287012dced2c1937dbdbf1b6ec2825a1eadd6f60f5180a593d"
+read -p "Input Hash : " inputHash
+read -p "Input VT Api Key : " apiKey
+curl -s -X GET https://www.virustotal.com/api/v3/search?query=$inputHash -H 'Content-Type: application/json' -H "x-apikey: $apiKey" | jq . > $inputHash.json
+
+fileName="$inputHash.json"
+hashID=$(jq -r '.data[]["id"]' $fileName)
+createDate=$(jq -r '.data[]["attributes"]["creation_date"]' $fileName | sed 's/^/\@/' | xargs date -d)
+lastChkDate=$(jq -r '.data[]["attributes"]["last_analysis_date"]' $fileName | sed 's/^/\@/' | xargs date -d)
+descr=$(jq -r '.data[]["attributes"]["magic"]' $fileName)
+names=$( jq -r '.data[]["attributes"]["names"][]' $fileName)
+crowdYaraRuleID=$(jq -r '.data[]["attributes"]["crowdsourced_yara_results"][]["ruleset_id"]' $fileName)
+crowdYaraRuleName=$(jq -r '.data[]["attributes"]["crowdsourced_yara_results"][]["rule_name"]' $fileName)
+crowdYaraRuleDesc=$(jq -r '.data[]["attributes"]["crowdsourced_yara_results"][]["description"]' $fileName)
+crowdYaraRulematchDate=$(jq -r '.data[]["attributes"]["crowdsourced_yara_results"][]["match_date"]' $fileName | sed 's/^/\@/' | xargs date -d)
+crowdYaraSource=$(jq -r '.data[]["attributes"]["crowdsourced_yara_results"][]["source"]' $fileName)
+#sandboxVerdictZenboxCat=$(jq -r '.data[]["attributes"]["sandbox_verdicts"]["Zenbox"]["category"]' $fileName)
+lastAnalysisStatsMal=$(jq -r '.data[]["attributes"]["last_analysis_stats"]["malicious"]' $fileName)
+lastAnalysisStatsSus=$(jq -r '.data[]["attributes"]["last_analysis_stats"]["suspicious"]' $fileName)
+lastAnalysisStatsUnd=$(jq -r '.data[]["attributes"]["last_analysis_stats"]["undetected"]' $fileName)
+lastAnalysisStatsHar=$(jq -r '.data[]["attributes"]["last_analysis_stats"]["harmless"]' $fileName)
+lastAnalysisStatsTim=$(jq -r '.data[]["attributes"]["last_analysis_stats"]["timeout"]' $fileName)
+lastAnalysisStatsCon=$(jq -r '.data[]["attributes"]["last_analysis_stats"]["confirmed-timeout"]' $fileName)
+lastAnalysisStatsFai=$(jq -r '.data[]["attributes"]["last_analysis_stats"]["failure"]' $fileName)
+lastAnalysisStatsTypUns=$(jq -r '.data[]["attributes"]["last_analysis_stats"]["type-unsupported"]' $fileName)
+
+echo "=====VIRUS TOTAL HASH OUTPUT====="
+echo "Hashing : $hashID"
+echo "Create Date : $createDate"
+echo "Last Analysis Date : $lastChkDate"
+echo "Description : $descr"
+#echo "File Names : $names"
+echo "================================="
+echo "===CROWDSOURCED YARA RESULTS====="
+echo "================================="
+echo "Ruleset ID : $crowdYaraRuleID"
+echo "Rule Name : $crowdYaraRuleName"
+echo "Description : $crowdYaraRuleDesc"
+echo "Match Date : $crowdYaraRulematchDate"
+echo "Source : $crowdYaraSource"
+echo "================================="
+echo "=====LAST ANALYSIS STATS========="
+echo "================================="
+echo "Malicious : $lastAnalysisStatsMal"
+echo "Suspicious : $lastAnalysisStatsSus"
+echo "Undetected : $lastAnalysisStatsUnd"
+echo "Harmless : $lastAnalysisStatsHar"
+echo "Timeout : $lastAnalysisStatsTim"
+echo "Confirmed-Timeout : $lastAnalysisStatsCon"
+echo "Failure : $lastAnalysisStatsFai"
+echo "Type Unsupported : $lastAnalysisStatsTypUns"
